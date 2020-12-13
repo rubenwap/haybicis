9c4629d05c36e0ab7fe3b39d08a0840321986967
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-12-13 11:21:26 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -25,6 +25,18 @@ import requests
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+PERMISSIONS = ['read::alexa:device:all:address']
+NOTIFY_MISSING_PERMISSIONS = 'Por favor, activa el permiso de localización en la app de Alexa.'
+BIKES_AVAILABLE = "Hay {mechanical} bicis mecánicas y {ebike} eléctricas."""
+HELP_REQUEST = "Qué necesitas?"
+BYE = "Adios!"
+GENERIC_EXCEPTION = "Perdona pero no te acabo de entender"
+REFLECTOR = "Acabas de lanzar {}"
+NO_ADDRESS = 'It looks like you don\'t have an address set. You can set your address from the companion app.'
+ADDRESS_AVAILABLE = 'Here is your full address: {}, {}, {}'
+ERROR = 'There was an error with the skill. Please check the logs.'
+LOCATION_FAILURE = 'There was an error with the Device Address API.'
+
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
     def can_handle(self, handler_input):
@@ -43,33 +55,49 @@ class LaunchRequestHandler(AbstractRequestHandler):
                 .response
         )
 
-
-# https://github.com/alexa/alexa-cookbook/blob/master/feature-demos/skill-demo-device-location/lambda/py/lambda_function.py
-
 class HayBicisIntentHandler(AbstractRequestHandler):
     """Handler for HayBicis Intent."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("HayBicis")(handler_input)
+        
+        return is_intent_name("HayBicis")(handler_input)
 
     def handle(self, handler_input):
 
         # type: (HandlerInput) -> Response
+        logger.info("In HayBicisIntentHandler")
+        service_client_fact = handler_input.service_client_factory
+        response_builder = handler_input.response_builder
 
-        # service_client_fact = handler_input.service_client_factory
-        # device_id = get_device_id(handler_input)
-        # device_addr_client = service_client_fact.get_device_address_service()
-        # addr = device_addr_client.get_full_address(device_id)
-        # logger.info(addr)
-        bikes_available = self.get_bikes(244)
-        speak_output = bikes_available
+        if not (get_api_access_token(handler_input)):
+            logger.info("no api access token")
+            response_builder.speak(NOTIFY_MISSING_PERMISSIONS)
+            response_builder.set_card(
+                AskForPermissionsConsentCard(permissions=PERMISSIONS))
+            return response_builder.response
 
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
+        try:
+            device_id = get_device_id(handler_input)
+            device_addr_client = service_client_fact.get_device_address_service()
+            addr = device_addr_client.get_full_address(device_id)
+
+            logger.info('Location API response retrieved, now building response')
+            logger.info(addr)
+            if addr.address_line1 is None and addr.state_or_region is None:
+                response_builder.speak(NO_ADDRESS)
+            else:
+                bikes_available = self.get_bikes(244)
+                speak_output = bikes_available
+                
+                response_builder.speak(ADDRESS_AVAILABLE.format(
+                    addr.address_line1, addr.state_or_region, addr.postal_code) + speak_output)
+            return response_builder.response
+        except ServiceException as e:
+            logger.error("error reported by device location service")
+            raise e
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            return handler_input.response_builder.speak(ERROR)
 
 
     # def get_closest_bicing_id():
@@ -80,7 +108,34 @@ class HayBicisIntentHandler(AbstractRequestHandler):
         # station information: https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information
         # TODO: Remove hardcoded station and pass the argument. Have to find out how to set personal settings in Alexa
         available_bikes = list(filter(lambda item: item["station_id"] == station_id, resp.json()["data"]["stations"]))[0]["num_bikes_available_types"]
-        return f"""Hay {available_bikes["mechanical"]} bicis mecánicas y {available_bikes["ebike"]} eléctricas."""
+        return BIKES_AVAILABLE.format(mechanical=available_bikes["mechanical"], ebike=available_bikes["ebike"])
+
+class HayBicisErrorHandler(AbstractExceptionHandler):
+    """Catch getAddress error handler, log exception and
+    respond with custom message.
+    """
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return (isinstance(exception, ServiceException))
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.info("In GetAddressErrorHandler")
+        logger.error(exception , exc_info=True)
+
+        if (exception.status_code==403):
+            return (handler_input.response_builder
+                .speak(NOTIFY_MISSING_PERMISSIONS)
+                .set_card(
+                    AskForPermissionsConsentCard(permissions=PERMISSIONS))
+                .response
+            )
+
+        # not a permissions issue, so just return general failure
+        return (handler_input.response_builder
+            .speak(LOCATION_FAILURE)
+            .response
+        )
 
 
 class HelpIntentHandler(AbstractRequestHandler):
@@ -178,6 +233,24 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
                 .response
         )
 
+
+# Request and Response loggers
+class RequestLogger(AbstractRequestInterceptor):
+    """Log the alexa requests."""
+    def process(self, handler_input):
+        # type: (HandlerInput) -> None
+        logger.debug("Alexa Request: {}".format(
+            handler_input.request_envelope.request))
+
+
+class ResponseLogger(AbstractResponseInterceptor):
+    """Log the alexa responses."""
+    def process(self, handler_input, response):
+        # type: (HandlerInput, Response) -> None
+        logger.debug("Alexa Response: {}".format(response))
+
+
+
 # The SkillBuilder object acts as the entry point for your skill, routing all request and response
 # payloads to the handlers above. Make sure any new handlers or interceptors you've
 # defined are included below. The order matters - they're processed top to bottom.
@@ -191,6 +264,12 @@ sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
 sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+# Register exception handlers
+sb.add_exception_handler(HayBicisErrorHandler())
+
+# TODO: Uncomment the following lines of code for request, response logs.
+sb.add_global_request_interceptor(RequestLogger())
+sb.add_global_response_interceptor(ResponseLogger())
 
 sb.add_exception_handler(CatchAllExceptionHandler())
 
@@ -225,13 +304,7 @@ lambda_handler = sb.lambda_handler()
 # sb = CustomSkillBuilder(api_client=DefaultApiClient())
 
 
-# PERMISSIONS = ['read::alexa:device:all:address']
-# NOTIFY_MISSING_PERMISSIONS = 'Por favor, activa el permiso de localización en la app de Alexa.'
-# BIKES_AVAILABLE = "Hay {mechanical} bicis mecánicas y {ebike} eléctricas."""
-# HELP_REQUEST = "Qué necesitas?"
-# BYE = "Adios!"
-# GENERIC_EXCEPTION = "Perdona pero no te acabo de entender"
-# REFLECTOR = "Acabas de lanzar {}"
+
 
 
 # # https://github.com/alexa/alexa-cookbook/blob/master/feature-demos/skill-demo-device-location/lambda/py/lambda_function.py
@@ -255,88 +328,9 @@ lambda_handler = sb.lambda_handler()
 #         )
 
 
-# class HayBicisIntentHandler(AbstractRequestHandler):
-#     """Handler for HayBicis Intent."""
-#     def can_handle(self, handler_input):
-#         # type: (HandlerInput) -> bool
-        
-#         return is_intent_name("HayBicis")(handler_input)
-
-#     def handle(self, handler_input):
-
-#         # type: (HandlerInput) -> Response
-#         logger.info("In HayBicisIntentHandler")
-#         service_client_fact = handler_input.service_client_factory
-#         response_builder = handler_input.response_builder
-
-#         if not (get_api_access_token(handler_input)):
-#             logger.info("no api access token")
-#             response_builder.speak(NOTIFY_MISSING_PERMISSIONS)
-#             response_builder.set_card(
-#                 AskForPermissionsConsentCard(permissions=PERMISSIONS))
-#             return response_builder.response
-
-#         try:
-#             device_id = get_device_id(handler_input)
-#             device_addr_client = service_client_fact.get_device_address_service()
-#             addr = device_addr_client.get_full_address(device_id)
-
-#             logger.info('Location API response retrieved, now building response')
-#             logger.info(addr)
-#             if addr.address_line1 is None and addr.state_or_region is None:
-#                 response_builder.speak(NO_ADDRESS)
-#             else:
-#                 bikes_available = self.get_bikes(244)
-#                 speak_output = bikes_available
-                
-#                 response_builder.speak(ADDRESS_AVAILABLE.format(
-#                     addr.address_line1, addr.state_or_region, addr.postal_code) + speak_output)
-#             return response_builder.response
-#         except ServiceException as e:
-#             logger.error("error reported by device location service")
-#             raise e
-#         except Exception as e:
-#             logger.error(e, exc_info=True)
-#             return handler_input.response_builder.speak(ERROR)
 
 
-#     # def get_closest_bicing_id():
-#     #     pass
 
-#     def get_bikes(self, station_id):
-#         resp = requests.get("https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_status")
-#         # station information: https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information
-#         # TODO: Remove hardcoded station and pass the argument. Have to find out how to set personal settings in Alexa
-#         available_bikes = list(filter(lambda item: item["station_id"] == station_id, resp.json()["data"]["stations"]))[0]["num_bikes_available_types"]
-#         return BIKES_AVAILABLE.format(mechanical=available_bikes["mechanical"], ebike=available_bikes["ebike"])
-
-
-# class HayBicisErrorHandler(AbstractExceptionHandler):
-#     """Catch getAddress error handler, log exception and
-#     respond with custom message.
-#     """
-#     def can_handle(self, handler_input, exception):
-#         # type: (HandlerInput, Exception) -> bool
-#         return (isinstance(exception, ServiceException))
-
-#     def handle(self, handler_input, exception):
-#         # type: (HandlerInput, Exception) -> Response
-#         logger.info("In GetAddressErrorHandler")
-#         logger.error(exception , exc_info=True)
-
-#         if (exception.status_code==403):
-#             return (handler_input.response_builder
-#                 .speak(NOTIFY_MISSING_PERMISSIONS)
-#                 .set_card(
-#                     AskForPermissionsConsentCard(permissions=PERMISSIONS))
-#                 .response
-#             )
-
-#         # not a permissions issue, so just return general failure
-#         return (handler_input.response_builder
-#             .speak(LOCATION_FAILURE)
-#             .response
-#         )
 
 # class HelpIntentHandler(AbstractRequestHandler):
 #     """Handler for Help Intent."""
@@ -378,22 +372,6 @@ lambda_handler = sb.lambda_handler()
 #         logger.info("Session ended reason: {}".format(
 #             handler_input.request_envelope.request.reason))
 #         return handler_input.response_builder.response
-
-# # Request and Response loggers
-# class RequestLogger(AbstractRequestInterceptor):
-#     """Log the alexa requests."""
-#     def process(self, handler_input):
-#         # type: (HandlerInput) -> None
-#         logger.debug("Alexa Request: {}".format(
-#             handler_input.request_envelope.request))
-
-
-# class ResponseLogger(AbstractResponseInterceptor):
-#     """Log the alexa responses."""
-#     def process(self, handler_input, response):
-#         # type: (HandlerInput, Response) -> None
-#         logger.debug("Alexa Response: {}".format(response))
-
 
 # # Register intent handlers
 # sb.add_request_handler(LaunchRequestHandler())
