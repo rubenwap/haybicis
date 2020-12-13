936c153b416cea4ac770f0c1784fb0673cfd9292
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-12-13 22:39:16 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -76,7 +76,7 @@ class HayBicisIntentHandler(AbstractRequestHandler):
             else:
                 
                 geolocator = Nominatim(user_agent="hay-bicis")
-                address = "{}, Son Simonet, {}".format(addr.address_line1, addr.postal_code)
+                address = "{}, Barcelona, {}".format(addr.address_line1, addr.postal_code)
                 logger.info(address)
                 coordinates = geolocator.geocode(address)
                 
