6a57c1d6a5433fa3f82bab5c5b79fef919df201d
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-12-13 11:10:04 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -94,7 +94,7 @@ class HayBicisIntentHandler(AbstractRequestHandler):
         return BIKES_AVAILABLE.format(mechanical=available_bikes["mechanical"], ebike=available_bikes["ebike"])
 
 
-class GetAddressErrorHandler(AbstractExceptionHandler):
+class HayBicisErrorHandler(AbstractExceptionHandler):
     """Catch getAddress error handler, log exception and
     respond with custom message.
     """
@@ -121,101 +121,46 @@ class GetAddressErrorHandler(AbstractExceptionHandler):
             .response
         )
 
-
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = HELP_REQUEST
+        logger.info("In HelpIntentHandler")
 
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
-
+        handler_input.response_builder.speak(HELP).ask(HELP)
+        return handler_input.response_builder.response
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
     """Single handler for Cancel and Stop Intent."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                is_intent_name("AMAZON.StopIntent")(handler_input))
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = BYE
+        logger.info("In CancelOrStopIntentHandler")
 
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .response
-        )
-
+        handler_input.response_builder.speak(GOODBYE)
+        return handler_input.response_builder.response
 
 class SessionEndedRequestHandler(AbstractRequestHandler):
     """Handler for Session End."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+        return is_request_type("SessionEndedRequest")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
+        logger.info("In SessionEndedRequestHandler")
 
-        # Any cleanup logic goes here.
-
+        logger.info("Session ended reason: {}".format(
+            handler_input.request_envelope.request.reason))
         return handler_input.response_builder.response
-
-
-class IntentReflectorHandler(AbstractRequestHandler):
-    """The intent reflector is used for interaction model testing and debugging.
-    It will simply repeat the intent the user said. You can create custom handlers
-    for your intents by defining them above, then also adding them to the request
-    handler chain below.
-    """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("IntentRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        intent_name = ask_utils.get_intent_name(handler_input)
-        speak_output = REFLECTOR.format(intent_name)
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
-
-
-class CatchAllExceptionHandler(AbstractExceptionHandler):
-    """Generic error handling to capture any syntax or routing errors. If you receive an error
-    stating the request handler chain is not found, you have not implemented a handler for
-    the intent being invoked or included it in the skill builder below.
-    """
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
-
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        logger.error(exception, exc_info=True)
-
-        speak_output = GENERIC_EXCEPTION
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
 
 # Request and Response loggers
 class RequestLogger(AbstractRequestInterceptor):
@@ -232,21 +177,19 @@ class ResponseLogger(AbstractResponseInterceptor):
         # type: (HandlerInput, Response) -> None
         logger.debug("Alexa Response: {}".format(response))
 
-# The SkillBuilder object acts as the entry point for your skill, routing all request and response
-# payloads to the handlers above. Make sure any new handlers or interceptors you've
-# defined are included below. The order matters - they're processed top to bottom.
 
-
-sb.add_request_handler(LaunchRequestHandler())
+# Register intent handlers
 sb.add_request_handler(HayBicisIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
-sb.add_exception_handler(GetAddressErrorHandler())
-sb.add_exception_handler(CatchAllExceptionHandler())
 
-sb.add_global_request_interceptor(RequestLogger())
-sb.add_global_response_interceptor(ResponseLogger())
+# Register exception handlers
+sb.add_exception_handler(HayBicisErrorHandler())
 
+# TODO: Uncomment the following lines of code for request, response logs.
+# sb.add_global_request_interceptor(RequestLogger())
+# sb.add_global_response_interceptor(ResponseLogger())
+
+# Handler name that is used on AWS lambda
 lambda_handler = sb.lambda_handler()
